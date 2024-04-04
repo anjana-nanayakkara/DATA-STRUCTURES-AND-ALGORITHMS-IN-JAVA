2eb9ddfb58be060ddb3b247cbeefda0e6120b20e
“anjana <74060929+anjana-nanayakkara@users.noreply.github.com>
2024-04-04 17:36:04 +0530

    sorting codes practice and added

---
+++ Array/SelectionSort.java
@@ -0,0 +1,35 @@
+public class SelectionSort {
+
+    public static void main(String args[])
+    {
+        SelectionSort ob = new SelectionSort();
+        int arr[] = {64,25,12,22,11};
+        ob.selectionSort(arr);
+        System.out.println("Sorted array");
+
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+    private void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
